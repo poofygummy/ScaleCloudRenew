d098ab11a6272935c273c5fdd62167c076a01c81
PoofyGummy <poofygummy@gmail.com>
2026-06-25 19:33:53 +0200

    Remove extension OSLog to fix OSLog.OSLogEntryLog.Level ambiguity in swiftinterface

--- Sources/Extensions/OSLog+SideStore.swift
+++ Sources/Extensions/OSLog+SideStore.swift
@@ -13,35 +13,6 @@ public let customLog = OSLog(subsystem: "org.sidestore.sidestore",
                       category: "ios")
 
 
-public extension OSLog {
-    /// Error logger extension
-    /// - Parameters:
-    ///   - message: String or format string
-    ///   - args: optional args for format string
-    @inlinable
-    static func error(_ message: StaticString, _ args: CVarArg...) {
-        os_log(message, log: customLog, type: .error, args)
-    }
-    
-    /// Info logger extension
-    /// - Parameters:
-    ///   - message: String or format string
-    ///   - args: optional args for format string
-    @inlinable
-    static func info(_ message: StaticString, _ args: CVarArg...) {
-        os_log(message, log: customLog, type: .info, args)
-    }
-    
-    /// Debug logger extension
-    /// - Parameters:
-    ///   - message: String or format string
-    ///   - args: optional args for format string
-    @inlinable
-    static func debug(_ message: StaticString, _ args: CVarArg...) {
-        os_log(message, log: customLog, type: .debug, args)
-    }
-}
-
 // TODO: Add file,line,function to messages? -- @JoeMatt
 
 /// Error logger convenience method for SideStore logging
@@ -50,7 +21,7 @@ public extension OSLog {
 ///   - args: optional args for format string
 @inlinable
 public func ELOG(_ message: StaticString, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, _ args: CVarArg...) {
-    OSLog.error(message, args)
+    os_log(message, log: customLog, type: .error, args)
 }
 
 /// Info logger convenience method for SideStore logging
@@ -59,7 +30,7 @@ public func ELOG(_ message: StaticString, file: StaticString = #file, function: 
 ///   - args: optional args for format string
 @inlinable
 public func ILOG(_ message: StaticString, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, _ args: CVarArg...) {
-    OSLog.info(message, args)
+    os_log(message, log: customLog, type: .info, args)
 }
 
 /// Debug logger convenience method for SideStore logging
@@ -68,7 +39,7 @@ public func ILOG(_ message: StaticString, file: StaticString = #file, function: 
 ///   - args: optional args for format string
 @inlinable
 public func DLOG(_ message: StaticString, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, _ args: CVarArg...) {
-    OSLog.debug(message, args)
+    os_log(message, log: customLog, type: .debug, args)
 }
 
 // mark: Helpers
